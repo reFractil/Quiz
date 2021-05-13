ea9c065d57f6fc91cb8c2ea2652c545476c34f9a
reFractil <efra15@hotmail.com>
2021-05-13 14:20:51 -0500

    First Commit

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    private let questions: [String] = [
+        "What is 7+7?",
+        "What is the capital of Vermont?",
+        "What is cognac made from?"
+    ]
+    
+    private let answers: [String] = [
+        "14",
+        "Montpelier",
+        "Grapes"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        questionLabel.text = questions[currentQuestionIndex]
     }
-
-
+    
+    private var currentQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+        
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+        
+    }
+    
 }
 
